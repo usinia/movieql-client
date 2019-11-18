7f1ae68de463c08f1d33760c6223591f924b64cc
usinia <usinia0103@gmail.com>
2019-11-18 22:35:30 +0900

    #7 Creating a Query with variables

--- src/Detail.js
+++ src/Detail.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { Query } from "react-apollo";
+import { MOVIE_DETAILS } from "./queries";
 
-const Detail = ({ match: { params: { movieId } } }) => {
-  console.log(movieId);
-  return <div>detail</div>;
-};
+const Detail = ({ match: { params: { movieId } } }) =>
+  <Query query={MOVIE_DETAILS} variables={{ movieId }}>
+    {({ loading, data, error }) => {
+      if (loading) return "loading";
+      if (error) return "error";
+      return (
+        <React.Fragment>
+          {data.movie.title}
+          {data.movie.medium_cover_image}
+          {data.movie.rating}
+          {data.movie.description_intro}
+        </React.Fragment>
+      );
+    }}
+  </Query>;
 
 export default Detail;
--- src/queries.js
+++ src/queries.js
@@ -11,3 +11,23 @@ export const HOME_PAGE = gql`
     }
   }
 `;
+
+export const MOVIE_DETAILS = gql`
+  query getMovieDetail($movieId: Int!) {
+    movie(id: $movieId) {
+      title
+      rating
+      description_intro
+      language
+      medium_cover_image
+      genres
+    }
+    suggestions(id: $movieId) {
+      title
+      description_intro
+      medium_cover_image
+    }
+  }
+`;
+// line 16: query getMovieDetail() => for apollo
+// line 17: movie() => for graphql server
