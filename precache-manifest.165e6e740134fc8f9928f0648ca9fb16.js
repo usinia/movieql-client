1eabd63bf49011a44cae335361c59d391b0a0dc2
usinia <usinia0103@gmail.com>
2019-11-18 23:19:17 +0900

    Updates

---
+++ precache-manifest.165e6e740134fc8f9928f0648ca9fb16.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df006408eb95c2236de63d5c0f240942",
+    "url": "/movieql-client/index.html"
+  },
+  {
+    "revision": "3c69f48e60121ed3846c",
+    "url": "/movieql-client/static/js/2.bb286d4c.chunk.js"
+  },
+  {
+    "revision": "9b01c9ad2b9f9da2d1a9",
+    "url": "/movieql-client/static/js/main.58e69ff8.chunk.js"
+  },
+  {
+    "revision": "a969015583a992255851",
+    "url": "/movieql-client/static/js/runtime-main.371cc89d.js"
+  }
+]);
